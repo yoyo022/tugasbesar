65d1295f86f74fff86575edd919a75c951be29e0
yoyo022 <yoyo022@users.noreply.github.com>
2019-11-17 13:13:45 +0700

    TugasBesar

---
+++ src/com/tubes/controller/MainFormController.java
@@ -0,0 +1,82 @@
+package com.tubes.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class MainFormController {
+
+    @FXML
+    private ImageView img1;
+    @FXML
+    private ImageView img2;
+    @FXML
+    private ImageView img3;
+    @FXML
+    private ImageView img4;
+    @FXML
+    private ImageView img5;
+    @FXML
+    private ImageView img6;
+    @FXML
+    private ImageView img7;
+    @FXML
+    private ImageView img8;
+    @FXML
+    private ImageView img9;
+    @FXML
+    private ImageView img10;
+    @FXML
+    private ImageView img11;
+    @FXML
+    private ImageView img12;
+
+
+    @FXML
+    private void img1Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img2Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img3Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img4Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img5Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img6Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img7Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img8Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img9Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img10Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img11Click(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    private void img12Click(MouseEvent mouseEvent) {
+    }
+}
